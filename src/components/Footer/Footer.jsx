90625494efe17975abcfadf9f07d26917659b060
Naman Mittal <mittalnaman08@gmail.com>
2023-09-02 12:10:03 +0530

    footer change

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="bg-gray-900  bottom-0 fixed   text-white w-full none">
       <div className="py-6 ml-20 mx-auto px-3 bg-gray-900 text-gray-400 text-sm">
-        &copy; {new Date().getFullYear()} BookClub Inc. All rights reserved.
+        &copy; {new Date().getFullYear()} BookMates Inc. All rights reserved.
       </div>
     </footer>
   );
